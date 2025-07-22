727e52b35ac89caf2918328aa4641510f5274cdc
KhaiaKaplan <mushkaplan770@gmail.com>
2025-07-22 17:32:54 +0300

    Initial commit

---
+++ src/test/java/com/the_internet/core/TestBase.java
@@ -0,0 +1,20 @@
+package com.the_internet.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestBase {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void init() {
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+}
